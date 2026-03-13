3ef318ce0e66a42d510b443b6045ceceb5e4a1e0
Vicky Bala <vikrambala.va@gmail.com>
2026-03-13 01:05:07 -0700

    temp wallpaper change

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -7,7 +7,7 @@
   # 直接从当前文件夹中读取配置文件作为配置内容
 
   # wallpaper, binary file  TODO: pretty mid this should be configured by-user?
-  home.file.".config/i3/wallpaper.jpg".source = ../../wallpapers/yotsubato.jpg;
+  home.file.".config/i3/wallpaper.jpg".source = ../../wallpapers/wallpaper.jpg;
   home.file.".config/i3/config".source = ./config;
   home.file.".config/i3/i3blocks.conf".source = ./i3blocks.conf;
   home.file.".config/i3/keybindings".source = ./keybindings;
