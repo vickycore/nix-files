502d82e5b575e32fa7bbf684c49610e811138ad2
Vicky Bala <vikrambala.va@gmail.com>
2026-03-14 11:06:17 -0700

    openssh support

--- modules/base.nix
+++ modules/base.nix
@@ -45,6 +45,20 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
+  
+  # TODO: ?
+  # networking.firewall.enable = false;
+
+  # Enable OpenSSH
+  services.openssh = {
+    enable = true;
+    settings = {
+      X11Forwarding = true;
+      PermitRootLogin = "no"; # disable root login
+      PasswordAuthentication = false; # disable password login
+    };
+    openFirewall = true;
+  }; 
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
--- users/vicky/nixos.nix
+++ users/vicky/nixos.nix
@@ -3,4 +3,11 @@
     # Add system-level modules here
     ../../modules/services/audio/navidrome.nix
   ];
+
+  # SSH public key
+  users.users.vicky = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAzx20jRavuXo9ozWHDJT5gjswqn7Eldxa0Dk+KJ6RGy vicky@chohept"
+    ];
+  };
 }
