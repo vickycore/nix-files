b1102b8bb2ba218e2f8a0f454cd7b0a7315e2ba5
Vicky Bala <vikrambala.va@gmail.com>
2026-03-14 10:22:11 -0700

    commenting out

--- hosts/thinkpad/seagate.nix
+++ hosts/thinkpad/seagate.nix
@@ -1,9 +1,9 @@
 { ... }:
 {
   # Prevent auto-mounting
-  services.udev.extraRules = ''
-    ENV{ID_FS_UUID}=="DAAE13C9010C6E56", ENV{UDISKS_IGNORE}="1"
-  '';
+  #services.udev.extraRules = ''
+  #  ENV{ID_FS_UUID}=="DAAE13C9010C6E56", ENV{UDISKS_IGNORE}="1"
+  #'';
 
   # Mounts my USB 2TB HDD to /mnt/storage
   # nofail -> boot succeeds even if HDD isn't plugged in
