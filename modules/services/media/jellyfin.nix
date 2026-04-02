f1202563ba61fb9ce39786a44e4b299a4badb4cc
Vicky Bala <vikrambala.va@gmail.com>
2026-04-01 17:31:27 -0700

    jellyfin access to external hard drives

--- modules/services/media/jellyfin.nix
+++ modules/services/media/jellyfin.nix
@@ -1,10 +1,13 @@
-{ config, pkgs, ... }:
+{ config, pkgs, username,  ... }:
 
 {
   services.jellyfin = {
     enable = true;
     openFirewall = true;
     # Default ports: 8096, 8920 (https://jellyfin.org/docs/general/post-install/networking/#port-bindings)
+    user = "${username}";  # external drive access
+
+    # TODO: should be moved to hosts?
     dataDir = "/mnt/storage/jellyfin/data/";
     cacheDir = "/mnt/storage/jellyfin/cache/";
   };
